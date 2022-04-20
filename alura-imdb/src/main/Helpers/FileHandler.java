8fd1b243b3d838552c56a9066386e900444ad056
Jonathan Willian <jonathanwc.pinheiro@outlook.com>
2022-04-19 22:11:38 -0300

    day one: the project must get top 250 movies from Imdb API using java.net.http library

---
+++ alura-imdb/src/main/Helpers/FileHandler.java
@@ -0,0 +1,20 @@
+package main.Helpers;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class FileHandler {
+    public static String getApiKeyFromConfigFile() throws Exception {
+        final var workingDirectory = System.getProperty("user.dir");
+        final var file = new File(workingDirectory + "\\alura-imdb\\src\\main\\config\\api-key.txt");
+
+        final var bufferedReader = new BufferedReader(new FileReader(file));
+
+        try {
+            return bufferedReader.readLine();
+        } catch (Exception ex) {
+            throw new Exception("text file from 'src/main/config/api-key.txt' should contain public api key from IMDb");
+        }
+    }
+}
---
+++ alura-imdb/src/main/get250ImdbMovies.java
@@ -0,0 +1,25 @@
+package main;
+
+import main.Helpers.FileHandler;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class get250ImdbMovies {
+    public static void main(String[] args) throws Exception {
+        String apiKey = FileHandler.getApiKeyFromConfigFile();
+
+        final var imdbUri = "https://imdb-api.com/en/api/top250movies/" + apiKey;
+
+        final var client = HttpClient.newHttpClient();
+        final var request = HttpRequest
+                .newBuilder(new URI(imdbUri))
+                .GET()
+                .build();
+
+        final var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
+}
